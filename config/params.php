9713f818b37e9d0575d4f22763dce81df8d79001
MarcoAntonioMeza <marcosmeza01@outlook.com>
2025-09-30 18:58:19 -0600

    Send sms

--- config/params.php
+++ config/params.php
@@ -43,7 +43,7 @@ return [
      * Email used in contact form.
      * Users will send you emails to this address.
      */
-    'adminEmail' => 'promocion@cresca.com.mx',
+    'adminEmail' => 'huatulco.conecta@lercomx.com',
     
 
     /**
